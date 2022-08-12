d78fc4b99950484785ecf0d5caf008af886fee21
vikram negi <vikramnegi175@gmail.com>
2022-08-13 01:21:24 +0530

    completing upper section of home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:ffi';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,6 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -29,26 +33,126 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: Column(
-          children: const [
-            Icon(Icons.location_on),
-          ],
-        ),
-        title: Text(widget.title),
+        body: SafeArea(
+            child: Container(
+      color: Colors.white,
+      child: Column(
+        children: [
+          upperSection(context),
+          const SizedBox(
+            height: 15,
+          ),
+          Stack(children: [
+            Container(
+              child: Image.asset(
+                "lib/assets/images/Group 8131.jpg",
+                scale: 0.95,
+              ),
+            ),
+            Positioned(
+              bottom: 8,
+              left : 130,
+              child: Container(
+                
+                width: MediaQuery.of(context).size.width*0.35,
+                child: CupertinoSearchTextField(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                      borderRadius: BorderRadius.circular(15),
+                      ),
+                  itemSize: 10,
+                  prefixIcon: const Icon(
+                    CupertinoIcons.search,
+                    size: 15,
+                  ),
+                  padding: const EdgeInsets.only(bottom:0, left: 5, top: 3),
+                  placeholder: "Search your courses",
+                  placeholderStyle: const TextStyle(
+                    fontSize: 10,
+                  ),
+                ),
+              ),
+            ),
+          ])
+        ],
       ),
-    
-    );
+    )));
+  }
+
+  Container upperSection(BuildContext context) {
+    return Container(
+        decoration: const BoxDecoration(color: Colors.white, boxShadow: [
+          BoxShadow(
+            offset: Offset(0, 1),
+            color: Color.fromARGB(255, 201, 200, 200),
+            blurRadius: 5,
+          )
+        ]),
+        child: Column(
+          children: [
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // const SizedBox(height: 10,),
+                  Row(
+                    children: const [
+                      Icon(Icons.location_on),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        "Sector-44, Real Estate, Sector- 62, Gurugram",
+                        style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.w500,
+                            color: Color.fromARGB(220, 65, 88, 89)),
+                      ),
+                    ],
+                  ),
+                  const Icon(Icons.keyboard_arrow_down)
+                ],
+              ),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.80,
+                    child: CupertinoSearchTextField(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          border: Border.all(color: Colors.grey)),
+                      itemSize: 35,
+                      prefixIcon: const Icon(
+                        CupertinoIcons.search,
+                        size: 25,
+                      ),
+                      suffixIcon: const Icon(
+                        CupertinoIcons.xmark_circle_fill,
+                        size: 25,
+                      ),
+                      padding:
+                          const EdgeInsets.only(bottom: 10, left: 5, top: 10),
+                      placeholder: "Search for a serive",
+                    ),
+                  ),
+                  const Icon(
+                    Icons.whatsapp,
+                    size: 35,
+                  ),
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
